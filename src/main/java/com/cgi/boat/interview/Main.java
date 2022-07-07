c087bfcdcd370fe054ed5764c6df6f27a945ef93
tgaboo <t.gabor96@gmail.com>
2022-07-07 09:46:21 +0200

    perf: make local variables final

--- src/main/java/com/cgi/boat/interview/Main.java
+++ src/main/java/com/cgi/boat/interview/Main.java
@@ -10,8 +10,8 @@ public class Main {
     private static final int MOST_COMMON_FIRST_NAMES_LIMIT = 3;
 
     public static void main(String[] args) {
-        Map<String, List<String>> firstByLast = PeopleProcessor.firstnamesByLastname(PeopleSetup.people);
-        Map<String, List<String>> lastByFirst = PeopleProcessor.lastnamesByFirstname(PeopleSetup.people);
+        final Map<String, List<String>> firstByLast = PeopleProcessor.firstnamesByLastname(PeopleSetup.people);
+        final Map<String, List<String>> lastByFirst = PeopleProcessor.lastnamesByFirstname(PeopleSetup.people);
 
         System.out.println(MOST_COMMON_FIRST_NAMES_LIMIT + " most common first names are: ");
         lastByFirst.entrySet()
