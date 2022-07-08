6c08624f6764d74b26ec01232ad0dedd8f67da5d
viktor <viktor.revesz@t-systems.com>
2022-07-08 17:00:51 +0200

    Viktor Révész solution

--- src/main/java/com/cgi/boat/interview/Main.java
+++ src/main/java/com/cgi/boat/interview/Main.java
@@ -1,7 +1,9 @@
 package com.cgi.boat.interview;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class Main {
 
@@ -10,16 +12,19 @@ public class Main {
         Map<String, List<String>> lastByFirst = PeopleProcessor.lastnamesByFirstname(PeopleSetup.people);
 
         // TODO: Print out 3 most common first names along with number of occurrences
+        lastByFirst.entrySet()
+                .stream()
+                .collect(Collectors.toMap(
+                        Map.Entry::getKey,
+                        e -> e.getValue().size()))
+                .entrySet().stream()
+                .sorted(Collections.reverseOrder(Map.Entry.comparingByValue())).limit(3)
+                .forEach(elem->System.out.printf("%s: %s%n",elem.getKey(),elem.getValue()));
         // for example:
         // Homer: 32
         // Bart: 21
         // William: 3
     }
-
-
-
-
-
 
 
 }
