9a4e289415792d46bbf6197d73bfd508a9c03fb1
Csanyi Tamas <csanyi.tomi.c@gmail.com>
2022-07-31 10:27:20 +0200

    limit fix

--- src/main/java/com/cgi/boat/interview/Main.java
+++ src/main/java/com/cgi/boat/interview/Main.java
@@ -5,7 +5,7 @@ import java.util.Map;
 
 public class Main {
 
-    private static final int PRINT_LIMIT = 5;
+    private static final int PRINT_LIMIT = 3;
 
     public static void main(String[] args) {
         Map<String, List<String>> lastByFirst = PeopleProcessor.lastnamesByFirstname(PeopleSetup.people);
