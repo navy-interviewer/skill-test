b8966feb3505b4c49dceebbaf9a4d4779bdcc7fb
Zsizsik, Zoltán <zoltan.zsizsik@gmail.com>
2022-08-14 20:17:32 +0200

    simple positive test case

--- src/test/java/com/cgi/boat/interview/PeopleProcessorTest.java
+++ src/test/java/com/cgi/boat/interview/PeopleProcessorTest.java
@@ -3,11 +3,14 @@ package com.cgi.boat.interview;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
 class PeopleProcessorTest {
+
+    public static final List<Person> PEOPLE = Arrays.asList(new Person("John", "Doe"), new Person("John", "Silver"), new Person("Peter", "Doe"));
 
     @Test
     void lastnamesByFirstname() {
@@ -25,6 +28,12 @@ class PeopleProcessorTest {
     void firstnamesByLastname_input_is_empty_list() {
         Map<String, List<String>> map = PeopleProcessor.firstnamesByLastname(Collections.emptyList());
         Assertions.assertEquals(map.keySet().size(), 0);
+    }
+
+    @Test
+    void firstnamesByLastname_simple_list() {
+        Map<String, List<String>> map = PeopleProcessor.firstnamesByLastname(PEOPLE);
+        Assertions.assertEquals(map.keySet().size(), 2);
     }
 
 }
